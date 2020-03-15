fd6a03cd4379654b6784f7b0a425f2fff3e1ddc1
Georgy Gusev <goosebumpsmail2@gmail.com>
2020-03-15 23:50:21 +0300

    Добавили надзирателя за демонами

---
+++ overseer.php
@@ -0,0 +1,19 @@
+<?php
+
+$pid = $argv[1];
+
+while (1) {
+    if (!file_exists("/proc/{$pid}")) {
+        //exec('nohup php update.php > /dev/null 2>&1 &');
+        send_message(urlencode('Процесс ' . $pid . 'упал'));
+        die();
+    }
+    sleep(20);
+}
+
+function send_message($text, $chatId = -327595262){
+
+
+    file_get_contents("https://api.telegram.org/bot863176881:AAHBJ2IUoNAIkxv9fLKpOfQop5eQe9p68gk/sendMessage?chat_id=$chatId&text=$text");
+
+}
