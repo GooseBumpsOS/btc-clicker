88abcc6f62921ec4bbdf8bd6e770731353af4202
Georgy Gusev <goosebumpsmail2@gmail.com>
2020-03-16 23:03:17 +0300

    Исправил ошибку в shell скрипте

--- generator.sh
+++ generator.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-timeVar=date +%s;
+timeVar=`date +%s`;
 mkdir $timeVar;
 cp /var/www/clicker/mainWorker.php /var/www/clicker/prod/$timeVar;
 cp /var/www/clicker/balance-cli.php /var/www/clicker/prod/$timeVar;
-cd timeVar;
+cd $timeVar;
 php -f init.php
 rm init.php
