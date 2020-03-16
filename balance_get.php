46264e7d385f33ee9f3f4828978555da9679472f
Pavel <41327820+PL21RUS@users.noreply.github.com>
2020-03-16 18:28:10 +0300

    Добавлена остановка демона перед выводом средств
    Добавлена остановка демона перед выводом средств. Вывод вынесен в функцию.

--- balance_get.php
+++ balance_get.php
@@ -9,31 +9,30 @@ $ltc = 0;
 $bch = 0;
 
 for($i = 0; $i<count($files); $i++){
-	exec('nohup php /var/www/clicker/prod/'.$files[$i].'/balance-cli.php -c true > /dev/null 2>&1 &');
+	exec('nohup php /var/www/clicker/prod/'.$files[$i].'/balance-cli.php -c true > /var/www/clicker/prod/'.$files[$i].'/log.txt 2>&1 &');
+	$pid = file_get_contents('/var/www/clicker/prod/'.$files[$i].'/pid.txt');
 	$cash = json_decode(file_get_contents('/var/www/clicker/prod/'.$files[$i].'/balance.txt'),true);
+	
 	$btc = $cash[0]['@BitcoinClick_bot'];
 	$ltc = $cash[0]['@Litecoin_click_bot'];
 	$bch = $cash[0]['@BCH_clickbot'];
 
-	if($btc >= 0.00003){
-		exec('nohup php /var/www/clicker/prod/'.$files[$i].'/balance-cli.php -c @BitcoinClick_bot -a '.$btc.' > /dev/null 2>&1 &');
-		exec('nohup php /var/www/clicker/prod/'.$files[$i].'/balance-cli.php -c true > /dev/null 2>&1 &');
-		send_message(urlencode('С аккаунта: '.$files[$i].' было выведено: '.$btc.' BTC'));
-	}
-	if($ltc >= 0.0004){
-		exec('nohup php /var/www/clicker/prod/'.$files[$i].'/balance-cli.php -c @Litecoin_click_bot -a '.$ltc.' > /dev/null 2>&1 &');
-		exec('nohup php /var/www/clicker/prod/'.$files[$i].'/balance-cli.php -c true > /dev/null 2>&1 &');
-		send_message(urlencode('С аккаунта: '.$files[$i].' было выведено: '.$ltc.' LTC'));
-	}
-
-	if($bch >= 0.0001){
-		exec('nohup php /var/www/clicker/prod/'.$files[$i].'/balance-cli.php -c @BCH_clickbot -a '.$bch.' > /dev/null 2>&1 &');
-		exec('nohup php /var/www/clicker/prod/'.$files[$i].'/balance-cli.php -c true > /dev/null 2>&1 &');
-		send_message(urlencode('С аккаунта: '.$files[$i].' было выведено: '.$bch.' BCH'));
-	}
+	if($btc >= 0.00003)
+		checkBalance($pid, $files[$i], '@BitcoinClick_bot', $btc, 'BTC');
+	if($ltc >= 0.0004)
+		checkBalance($pid, $files[$i], '@Litecoin_click_bot', $ltc, 'LTC');
+	if($bch >= 0.0001)
+		checkBalance($pid, $files[$i], '@BCH_clickbot', $bch, 'BCH');
 	
 }
 
+function checkBalance($pid, $dir, $channel, $balance, $coin){
+	exec('kill -s SIGSTOP '.$pid);
+	exec('nohup php /var/www/clicker/prod/'.$dir.'/balance-cli.php -c '.$channel.' -a '.$balance.' > /var/www/clicker/prod/'.$dir.'/log.txt 2>&1 &');
+	exec('nohup php /var/www/clicker/prod/'.$dir.'/balance-cli.php -c true > /var/www/clicker/prod/'.$dir.'/log.txt 2>&1 &');
+	exec('kill -s SIGCONT '.$pid);
+	send_message(urlencode('С аккаунта: '.$dir.' было выведено: '.$balance.' '.$coin));
+}
 
 function send_message($text, $chatId = -327595262){
  file_get_contents("https://api.telegram.org/bot863176881:AAHBJ2IUoNAIkxv9fLKpOfQop5eQe9p68gk/sendMessage?chat_id=$chatId&text=$text");
